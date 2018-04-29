3dadea42c9bd2e9d7112650b081d6f5b2c4982ec
Srija S <srijakumar7@gmail.com>
2018-04-29 03:30:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ def self.alphabetical
   self.all.sort_by {|song| song.name}
 end
 
-def new_from_filename(fileName)
+def self.new_from_filename(fileName)
       parts = fileName.split(" - ")
       artist_name = parts[0]
       song_name = parts[1].gsub(".mp3", "")
